5dd5f27618d38b9da203afcdbd1f297512bf7673
Vadim Zinoviev <stragrus@mail.ru>
2021-08-14 13:41:46 +0300

    finish commit

--- webpack.config.js
+++ webpack.config.js
@@ -32,27 +32,18 @@ module.exports = {
           MiniCssExtractPlugin.loader, 'css-loader',          
         ],
       },
-      // {
-      //   test: /\.(png|jpe?g|gif)$/i,
-      //   use: [
-      //     {
-      //       loader: 'file-loader',
-      //       options: {
-      //         mimetype: 'image/png',
-      //         limit: false,
-      //         esModule: false,
-      //       },
-      //     },
-      //   ],
-      // },
       {
-        test: /\.(png|jpg|gif|webp)$/,
-        use: {
-          loader: 'url-loader',
-          options: {
-            limit: 8192, // будет использоваться file-loader
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              mimetype: 'image/png',
+              limit: false,
+              esModule: false,
+            },
           },
-        },
+        ],
       },
     ],
   },
