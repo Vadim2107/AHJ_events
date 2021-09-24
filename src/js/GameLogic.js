ade61b69f25a2a17b0bb754e61c1c2a343835fd2
Vadim2107 <58651870+Vadim2107@users.noreply.github.com>
2021-09-24 20:02:15 +0300

    third commit

--- src/js/GameLogic.js
+++ src/js/GameLogic.js
@@ -7,28 +7,49 @@ export default class GameLogic extends GameBoard {
     this.eventClick();
     this.dead = 0;
     this.lost = 0;
+    this.clickOrNo = true;
+  }
+
+  scoring(score) {
+    const dead = document.getElementById('dead');
+    const lost = document.getElementById('lost');
+    if (score) {
+      // eslint-disable-next-line no-plusplus
+      this.dead += 1;
+      dead.textContent = this.dead;
+    } else {
+    // eslint-disable-next-line no-plusplus
+      this.lost += 1;
+      lost.textContent = this.lost;
+      if (this.lost === 5) {
+        // eslint-disable-next-line no-alert
+        alert(`GAME OVER!!! Your account ${this.dead}`);
+        lost.textContent = 0;
+        dead.textContent = 0;
+        this.lost = 0;
+        this.dead = 0;
+      }
+    }
+    this.board.setNextCell();
   }
 
   eventClick() {
+    setInterval(() => {
+      if (this.clickOrNo) {
+        this.clickOrNo = false;
+      } else {
+        this.scoring(false);
+      }
+    }, 1000);
+
     this.board.cells.forEach((element) => {
-      element.addEventListener('click', (event) => {
-        const dead = document.getElementById('dead');
-        const lost = document.getElementById('lost');
+      element.addEventListener('click', () => {
+        this.clickOrNo = true;
         if (element.classList.contains('hole_has-goblin')) {
-          this.dead += 1;
-          dead.textContent = this.dead;
-          event.currentTarget.classList.remove('hole_has-goblin');
+          this.scoring(true);
+          this.board.setNextCell();
         } else {
-          this.lost += 1;
-          lost.textContent = this.lost;
-          if (this.lost === 5) {
-            // eslint-disable-next-line no-alert
-            alert(`GAME OVER!!! Your account ${this.dead}`);
-            lost.textContent = 0;
-            dead.textContent = 0;
-            this.lost = 0;
-            this.dead = 0;
-          }
+          this.scoring(false);
         }
       });
     });
